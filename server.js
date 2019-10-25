8fda7ac5f81444a0adce072546b36983095e411a
user2 <user2@bahaas-MacBook-Pro-2.local>
2019-10-25 16:02:53 +0400

    chore: prepare server for deployment

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const cards = require('./routes/api/cards');
-
-app.get('/', (req, res) => res.send('API running'));
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
 // Define routes
 app.use('/api/cards', cards);
+
+// Serve static assets in production
+if (process.env.NODE_ENV == 'production') {
+  // Set the static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
